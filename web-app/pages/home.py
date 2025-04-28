41037fcb7fe170d918dfaa871b22743ec4d775a3
shamaamahh <shamaamah.ahmad@gmail.com>
2025-04-28 17:23:22 -0400

    home page formatting

--- web-app/pages/home.py
+++ web-app/pages/home.py
@@ -2,53 +2,37 @@
 
 import streamlit as st
 
-
 def show_home():
     """This function displays the page elements for the home page"""
 
     # home page title
-    titlerow = st.columns([0.3, 0.4, 0.3])
+    titlerow = st.columns([1, 1, 1])
     with titlerow[1]:
         st.title("Let's play!")
 
+    st.write("")
+    st.write("")
+
     # show all game buttons
-    # row 1 - slots / blackjack
-    row1 = st.columns([0.2, 0.6, 0.2])
+    # row 1 - slots / blackjack / roulette
+    row1 = st.columns([1, 1, 1])
     with row1[0]:
 
         def cb_slots():
             st.session_state.current_page = "slots"
 
-        st.button("Slots", on_click=cb_slots)
+        st.button("Slots 🎰", on_click=cb_slots, use_container_width=True)
 
-    with row1[2]:
+    with row1[1]:
 
         def cb_blackjack():
             st.session_state.current_page = "blackjack"
 
-        st.button("Blackjack", on_click=cb_blackjack)
+        st.button("Blackjack 🃏", on_click=cb_blackjack, use_container_width=True)
 
-    # row 2 - roulette / game 1
-    row2 = st.columns([0.2, 0.2, 0.2, 0.2, 0.2])
-    with row2[1]:
+    with row1[2]:
 
         def cb_roulette():
             st.session_state.current_page = "roulette"
 
-        st.button("Roulette", on_click=cb_roulette)
-
-    with row2[3]:
-
-        def cb_game1():
-            st.session_state.current_page = "unknown_game1"
-
-        st.button("[Game 1]", on_click=cb_game1)
-
-    # row 3 - game 2
-    row3 = st.columns([0.35, 0.2, 0.35])
-    with row3[2]:
-
-        def cb_game2():
-            st.session_state.current_page = "unknown_game2"
-
-        st.button("[Game 2]", on_click=cb_game2)
+        st.button("Roulette 🎯", on_click=cb_roulette, use_container_width=True)
